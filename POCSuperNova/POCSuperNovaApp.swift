9cf0bc0319c02a0391ccab6b50dab60d1c3a0699
Eslm Salem <eslam.mohamedsalem@vodafone.com>
2024-02-12 13:17:43 +0200

    init

---
+++ POCSuperNova/POCSuperNovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCSuperNovaApp.swift
+//  POCSuperNova
+//
+//  Created by Eslam Salem on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct POCSuperNovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
